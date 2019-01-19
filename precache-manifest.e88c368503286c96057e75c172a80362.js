8e6207de3457fb407dc17dd20ad245dc25285ec5
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2019-01-19 14:24:32 -0500

    Updates

---
+++ precache-manifest.e88c368503286c96057e75c172a80362.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffaedfffee58f7da7dd4",
+    "url": "/waste-wizard-app/static/js/runtime~main.ffaedfff.js"
+  },
+  {
+    "revision": "679149406ac428483f8b",
+    "url": "/waste-wizard-app/static/js/main.67914940.chunk.js"
+  },
+  {
+    "revision": "9a71167e4aecc1b6df0a",
+    "url": "/waste-wizard-app/static/js/1.9a71167e.chunk.js"
+  },
+  {
+    "revision": "679149406ac428483f8b",
+    "url": "/waste-wizard-app/static/css/main.fcd065b4.chunk.css"
+  },
+  {
+    "revision": "399eeb948e1835690b7eb616a74ab4f5",
+    "url": "/waste-wizard-app/index.html"
+  }
+];
